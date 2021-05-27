96011fb45d4041db38cf6827e267b6aa6a3abe5b
薛景超 <2729011892@qq.com>
2021-05-27 23:01:33 +0800

    哈哈哈

---
+++ BingFaBianCheng/ThreadOne/StudyThread.java
@@ -0,0 +1,25 @@
+package ThreadOne;
+
+/*
+ * @author xjc
+ * @date 2021-05-27
+ * @desc 同时使用两种方式实现线程
+ */
+public class StudyThread {
+	public static void main(String[] args) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("我来自runnable");
+				//这个runnable传进去了，但是方法被重写了，所以不能得到实行
+			}
+		}) {
+			@Override
+			public void run() {
+				System.out.println("我来自thread");
+				//最终只有这行会运行
+			}
+		}.start();
+	}
+
+}
---
+++ ShujuJieGou/Array/StudyArray.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class StudyArray {
+	
+	/*
+	 * @author xjc
+	 * @date 2021-05-27
+	 * @desc 数组的初学习
+	 */
+	public static void main(String[] args) {
+		//第一种写法
+		int[] xjc = new int[10];
+		//第二种写法
+		int[] jjj = new int[] {1,2,3,4,5,6,7};
+		//二次封装我们自己的数组
+		
+	}
+	
+	/*
+	 * 数组可以快速的查询
+	 */
+
+}

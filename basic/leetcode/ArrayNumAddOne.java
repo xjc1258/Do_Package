29cb459e9e6c5841cc5520c4c6bddae972ef6c28
薛景超 <2729011892@qq.com>
2021-05-31 23:20:46 +0800

    数组的数字值加一

---
+++ basic/leetcode/ArrayNumAddOne.java
@@ -0,0 +1,27 @@
+package leetcode;
+/*@author xjc
+ * @date 2021-05-31
+ * @desc 数组组成的数字加1
+ */
+
+public class ArrayNumAddOne {
+
+	public static void main(String[] args) {
+		int[] xjc = new int[] { 1, 2, 3, 4, 5, 6 };
+		
+
+	}
+
+	public int[] plusOne(int[] digits) {
+		for (int i = digits.length - 1; i >= 0; i--) {
+			digits[i]++;
+			digits[i] = digits[i] % 10;
+			if (digits[i] != 0)
+				return digits;
+		}
+		digits = new int[digits.length + 1];
+		digits[0] = 1;
+		return digits;
+	}
+
+}

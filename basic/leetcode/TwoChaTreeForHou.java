70739dc451a1401e5eaa2b604d31271b43e0bc2d
薛景超 <2729011892@qq.com>
2021-06-06 22:11:45 +0800

    oo

---
+++ basic/leetcode/TwoChaTreeForHou.java
@@ -0,0 +1,28 @@
+package leetcode;
+/*
+ * @author xjc
+ * @date 2021-06-05
+ * @desc 二叉树的后序遍历
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoChaTreeForHou {
+
+	public List<Integer> postorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		postorder(root, res);
+		return res;
+	}
+
+	public void postorder(TreeNode root, List<Integer> res) {
+		if (root == null) {
+			return;
+		}
+		postorder(root.left, res);
+		postorder(root.right, res);
+		res.add(root.val);
+	}
+
+}
---
+++ basic/leetcode/TwoChaTreeForward.java
@@ -0,0 +1,29 @@
+package leetcode;
+/*
+ * @author xjc
+ * @date 2021-06-05
+ * @desc 二叉树的前序遍历
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoChaTreeForward {
+
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		preorder(root, res);
+		return res;
+	}
+
+	//递归遍历
+	public void preorder(TreeNode root, List<Integer> res) {
+		if (root == null) {
+			return;
+		}
+		res.add(root.val);
+		preorder(root.left, res);
+		preorder(root.right, res);
+	}
+
+}
